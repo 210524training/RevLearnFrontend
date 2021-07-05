5b30cfe387fe099e6ea27fc14bdaa080c8c1ceb6
dallinlemon <dallinlemon@gmail.com>
2021-07-05 16:32:09 -0600

    Updated package.json with new dependancies

--- RevLearnFrontend/components/WebNavBar.tsx
+++ RevLearnFrontend/components/WebNavBar.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {Text} from 'react-native'
+import {Text, View} from 'react-native'
 
 const NavBar = () => {
   return (
-    <>
+    <View>
       <Text>this in a nav bar</Text>
-    </>
+    </View>
   );
 }
 
