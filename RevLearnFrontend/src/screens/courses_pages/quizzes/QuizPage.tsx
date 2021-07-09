4ef7829b66645bb64478eff91aa8bc85fc4c5e8a
Sam Henley <samueljhenley@gmail.com>
2021-07-08 19:54:25 -0500

    create form for taking quizzes

--- RevLearnFrontend/src/screens/courses_pages/quizzes/QuizPage.tsx
+++ RevLearnFrontend/src/screens/courses_pages/quizzes/QuizPage.tsx
@@ -1,15 +1,147 @@
-import React from "react";
-import {Text} from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+import RadioForm from 'react-native-simple-radio-button';
+import ShortAnswerSubmission from '../../../components/quiz_submission/ShortAnswerSubmission';
+import MultipleChoiceQuizQuestion from '../../../models/MultipleChoiceQuizQuestion';
+import Quiz from '../../../models/Quiz';
+import QuizQuestion from '../../../models/QuizQuestion';
+import ShortAnswerQuestion from '../../../models/ShortAnswerQuestion';
+import { MultipleChoiceOption } from '../../../types/MyTypes';
 
 type Props = {
 
 }
-const QuisPage: React.FC<Props> = (props) => {
+
+const QuizPage: React.FC<Props> = (props) => {
+  
+  const [answer, setAnswer] = useState<string>();
+
+  const questions: (ShortAnswerQuestion | MultipleChoiceQuizQuestion)[] = [
+    {
+      questionID: '1',
+      questionTitle: 'Question 1',
+      pointValue: 1,
+      prompt: 'This is a question',
+      correctAnswer: 'answer1',
+    },
+    {
+      questionID: '2',
+      questionTitle: 'Question 2',
+      pointValue: 1,
+      prompt: 'This is a question',
+      correctAnswer: 'answer2',
+    },
+    {
+      questionID: '3',
+      questionTitle: 'Question 3',
+      pointValue: 1,
+      prompt: 'This is a question',
+      correctAnswer: 'answer3',
+    },
+    {
+      questionID: '4',
+      questionTitle: 'Question 4',
+      pointValue: 1,
+      prompt: 'This is a multiple choice question',
+      correctAnswer: 'OptionB',
+      choices: [
+        {option: 'OptionA', answer: 'The first option'},
+        {option: 'OptionB', answer: 'The second option'},
+        {option: 'OptionC', answer: 'The third option'},
+        {option: 'OptionD', answer: 'The fourth option'},
+      ]
+    },
+  ];
+
+  const quiz: Quiz = {
+    ID: '1',
+    title: 'Demo Quiz',
+    startDate: new Date(),
+    dueDate: new Date(),
+    description: 'This is a description of the quiz. Good luck.',
+    submissions: [],
+    passingGrade: 70,
+    questions,
+  }
+
+
+  // Creating an array of hooks that dynamically scales to match the number of questions
+  const answerHooks: {value: string | MultipleChoiceOption, setter: React.Dispatch<React.SetStateAction<any>> }[] = [];
+  quiz.questions.forEach( (question) => {
+    if (isMultipleChoiceQuestion(question)) {
+      const [ value, setter ] = useState<MultipleChoiceOption>('OptionA');
+      answerHooks.push( {value, setter});
+    } else {
+      const [ value, setter ] = useState<string>('OptionA');
+      answerHooks.push( {value, setter});
+    }
+  })
+
+  function isMultipleChoiceQuestion(question: QuizQuestion): question is MultipleChoiceQuizQuestion {
+    return "choices" in question;
+  }
+
+  function getRadioOptions(question: MultipleChoiceQuizQuestion): {label: string, value: string}[] {
+    const options = question.choices.map((choice) => {
+      return {label: choice.answer, value: choice.option}
+    });
+
+    return options;
+  }
+
+  // Grade the quiz by comparing the input answer with the corrrct answer 
+  const handleQuizSubmit = () => {
+    
+    let possiblePoints: number = 0;
+    questions.forEach( (question) => {
+      possiblePoints += question.pointValue;
+    })
+
+    let earnedPoints: number = 0;
+    
+    answerHooks.forEach( (question, index) => {
+      console.log(`Question ${index} answer: ${question.value}, expected ${questions[index].correctAnswer} `);
+      if(questions[index].correctAnswer === answerHooks[index].value) {
+        earnedPoints += questions[index].pointValue;
+      }
+    });
+
+    const percentCorrect = earnedPoints / possiblePoints;
+    const formattedPercent = Math.floor(percentCorrect * 10000) / 100;
+
+    console.log(`Earned ${earnedPoints} out of ${possiblePoints} points, or ${formattedPercent}%`);
+  }
+
   return (
     <>
-      <Text>QuisPage</Text>
+      <Text>{quiz.title}</Text>
+      <Text>{quiz.description}</Text>
+
+      {
+        quiz.questions.map( (question, index) => {
+          return (
+            <>
+            <View style={{borderWidth: 1}}>
+              <Text>{questions[index].questionTitle}</Text>
+              <Text>{questions[index].prompt}</Text>
+
+              { 
+                isMultipleChoiceQuestion(questions[index]) ? (
+                  <RadioForm radio_props={getRadioOptions(questions[index] as MultipleChoiceQuizQuestion)} initial={0} onPress={answerHooks[index].setter} />
+                ) : (
+                  <TextInput style={{borderWidth: 1}} onChangeText={answerHooks[index].setter} /> 
+                )
+              }
+
+            </View>
+            </>
+          )
+        })
+      }
+
+      <Button title='Submit Quiz' onPress={handleQuizSubmit} />
     </>
   )
 }
 
-export default QuisPage;
+export default QuizPage;
