ef8a87f11429d37f488f6881785457d4c84a84f4
zkao04 <hineskao@gmail.com>
2021-07-09 18:00:03 -0700

    Fixed eslint issue on DisplayCourseList

--- RevLearnFrontend/src/components/display_list/DisplayCourseList.tsx
+++ RevLearnFrontend/src/components/display_list/DisplayCourseList.tsx
@@ -1,42 +1,42 @@
-import React, { Dispatch, SetStateAction, useState} from 'react';
-import { Image, ImageSourcePropType, StyleSheet, Text, View } from 'react-native';
-import {Course} from '../../models/Course';
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import {
+  Image, ImageSourcePropType, StyleSheet, Text, View,
+} from 'react-native';
+import { Course } from '../../models/Course';
 
 type Props = {
   courses: Course[];
   setSelected: Dispatch<SetStateAction<string | undefined>>;
 }
 
-const DisplayCourseList: React.FC<Props> = ({courses, setSelected}) => {
-  return (
-    <>
-      {courses.map(element => (
+const DisplayCourseList: React.FC<Props> = ({ courses, setSelected }) => (
+  <>
+    {courses.map((element) => (
+      <View>
         <View>
-          <View>
-            <Text>Course ID:</Text> {`${element.courseID}`}
-          </View>
-          <View>
-            <Text>Course Title:</Text> {`${element.courseTitle}`}
-          </View>
-          <View>
-            <Text>Start Date:</Text> {`${element.startDate}`}
-          </View>
-          <View>
-            <Text>End Date:</Text> {`${element.endDate}`}
-          </View>
-          <View>
-            <Text>Teacher:</Text> {`${element.teacher}`}
-          </View>
-          <View>
-            <Text>Passing Grade:</Text> {`${element.passingGrade}`}
-          </View>
-          <View>
-            <Text>Category:</Text> {`${element.category}`}
-          </View>
+          <Text>Course ID:</Text> {`${element.courseID}`}
         </View>
-      ))}
-    </>
-  )}
+        <View>
+          <Text>Course Title:</Text> {`${element.courseTitle}`}
+        </View>
+        <View>
+          <Text>Start Date:</Text> {`${element.startDate}`}
+        </View>
+        <View>
+          <Text>End Date:</Text> {`${element.endDate}`}
+        </View>
+        <View>
+          <Text>Teacher:</Text> {`${element.teacher}`}
+        </View>
+        <View>
+          <Text>Passing Grade:</Text> {`${element.passingGrade}`}
+        </View>
+        <View>
+          <Text>Category:</Text> {`${element.category}`}
+        </View>
+      </View>
+    ))}
+  </>
+);
 
-
-export default DisplayCourseList; 
+export default DisplayCourseList;
