e28af1938680fb32c78a09b90e436a7582336ce1
Sam Henley <samueljhenley@gmail.com>
2021-07-10 13:43:08 -0500

    create skeleton for course grades page and components

---
+++ RevLearnFrontend/src/components/course_grades/CourseGradesStudentView.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Course } from '../../models/Course';
+import { User } from '../../models/User';
+
+type Props = {
+
+}
+
+const CourseGradesPage: React.FC<Props> = (props) => (
+  <>
+  </>
+);
+
+export default CourseGradesPage;
--- RevLearnFrontend/src/screens/courses_pages/CourseGradesPage.tsx
+++ RevLearnFrontend/src/screens/courses_pages/CourseGradesPage.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Text } from 'react-native';
+import CourseGradesStudentView from '../../components/course_grades/CourseGradesStudentView';
+import CourseGradesTeacherView from '../../components/course_grades/CourseGradesTeacherView';
 import WithCourseNavbar from '../../components/higher_order_components/Navbars/WithCourseNavbar';
+import { useAppSelector } from '../../hooks';
+import { selectUser, UserState } from '../../hooks/slices/user.slice';
+import { User } from '../../models/User';
 
 type Props = {
 
 }
-const CourseGradesPage: React.FC<Props> = (props) => (
-  <>
-    <Text>CourseGradesPage</Text>
-  </>
-);
+const CourseGradesPage: React.FC<Props> = () => {
+  const user: User = useAppSelector<UserState>(selectUser);
+
+  return (
+    <>
+      {
+        user.role === 'Student' ? (
+          <CourseGradesStudentView />
+        ) : (
+          <CourseGradesTeacherView />
+        )
+      }
+    </>
+  );
+};
 
 export default WithCourseNavbar(CourseGradesPage);
