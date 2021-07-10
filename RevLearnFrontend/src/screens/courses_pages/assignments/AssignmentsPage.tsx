f6ff919efed7ddfadc4d87fa4c3e5f2174bc7932
dallinlemon <dallinlemon@gmail.com>
2021-07-10 08:47:26 -0600

    Added create assignment button

--- RevLearnFrontend/src/screens/courses_pages/assignments/AssignmentsPage.tsx
+++ RevLearnFrontend/src/screens/courses_pages/assignments/AssignmentsPage.tsx
@@ -1,23 +1,32 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, Text } from 'react-native';
 import WithCourseNavbar from '../../../components/higher_order_components/Navbars/WithCourseNavbar';
 
 type Props = {
 
 }
-const AssignmentsPage: React.FC<Props> = (props) => (
-  <>
-    <Text>AssignmentsPage</Text>
-  </>
-);
+const AssignmentsPage: React.FC<Props> = (props) => {
+  const navigation = useNavigation();
+
+  const createNavHandler = () => {
+    navigation.navigate('CreateAssignmentPage');
+  };
+  return (
+    <>
+      <Text>AssignmentsPage</Text>
+      <Button title={'navigate to create'} onPress={createNavHandler}>Create Assignment</Button>
+    </>
+  );
+};
 
 // Navigate to SubmitAssignments, CreateAssignment, AssignmentsSubmissions, DetaliedSubmission
 /**
  * const navigation = useNavigation();
- * navigation.navigate('SubmitAssignments');
- * navigation.navigate('CreateAssignment');
- * navigation.navigate('AssignmentsSubmissions');
- * navigation.navigate('DetaliedSubmission');
+ * navigation.navigate('SubmitAssignmentsPage');
+ * navigation.navigate('CreateAssignmentPage');
+ * navigation.navigate('AssignmentsSubmissionsPage');
+ * navigation.navigate('DetaliedSubmissionPage');
  */
 
 export default WithCourseNavbar(AssignmentsPage);
