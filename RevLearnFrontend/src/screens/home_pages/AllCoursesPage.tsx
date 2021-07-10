6647adb7fca255acfa7e93e9cd89329854ccca04
dallinlemon <dallinlemon@gmail.com>
2021-07-10 08:35:26 -0600

    Fixed bugs

--- RevLearnFrontend/src/screens/home_pages/AllCoursesPage.tsx
+++ RevLearnFrontend/src/screens/home_pages/AllCoursesPage.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import {Text, ScrollView, Pressable, Platform, TouchableNativeFeedback, View} from 'react-native';
-import WithHomeNavbar from "../../components/higher_order_components/Navbars/WithHomeNavbar";
-import CoursesList from '../../components/display_list/DisplayCourseList'
+import React from 'react';
+import {
+  ScrollView, View,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import DisplayCourseList from "../../components/display_list/DisplayCourseList";
+import WithHomeNavbar from '../../components/higher_order_components/Navbars/WithHomeNavbar';
+import DisplayCourseList from '../../components/display_list/DisplayCourseList';
 import { courses } from '../../remote/RevLearnBackendAPI';
 
 type Props = {
@@ -11,18 +12,13 @@ type Props = {
 
 const AllCoursesPage: React.FC<Props> = (props) => {
   const nav = useNavigation();
-  const setSelected = () => {}
-  const handlePress = () => {
-    // nav.navigate({
-    //   course,
-    // });
-  }
+  const setSelected = () => { nav.canGoBack(); };
 
   return (
     <ScrollView>
       <View>
         {/* {
-          Platform.OS === 'android' ? 
+          Platform.OS === 'android' ?
           androidJSX() : coursesJSX()
         } */}
         {courses
