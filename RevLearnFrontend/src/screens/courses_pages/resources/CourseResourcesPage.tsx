ee010980b217463bf18f88514fc9e5d395c3bc76
dallinlemon <dallinlemon@gmail.com>
2021-07-11 16:49:10 -0600

    Added navigation functionality from course resources to detailed resource pages

--- RevLearnFrontend/src/screens/courses_pages/resources/CourseResourcesPage.tsx
+++ RevLearnFrontend/src/screens/courses_pages/resources/CourseResourcesPage.tsx
@@ -1,15 +1,23 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Button } from 'react-native';
 import WithCourseNavbar from '../../../components/higher_order_components/Navbars/WithCourseNavbar';
 
 type Props = {
 
 }
-const CourseResourcesPage: React.FC<Props> = (props) => (
-  <>
-    <Text>CourseResourcesPage</Text>
-  </>
-);
+const CourseResourcesPage: React.FC<Props> = (props) => {
+  const navigation = useNavigation();
+  const temp = () => {
+    navigation.navigate('AddCourseResourcePage');
+  };
+  return (
+    <>
+      <Text>CourseResourcesPage</Text>
+      <Button title={'Resources'} onPress={temp}/>
+    </>
+  );
+};
 
 // Navigate to DetailedResource, AddCourseResource
 /**
