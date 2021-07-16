b5fe0b8ec50f8c5b9a1bedcff34e39595c9bc835
dallinlemon <dallinlemon@gmail.com>
2021-07-15 20:13:08 -0600

    fix logic problem in display course grades

--- RevLearnFrontend/src/components/course_grades/CourseGradesStudentView.tsx
+++ RevLearnFrontend/src/components/course_grades/CourseGradesStudentView.tsx
@@ -3,47 +3,40 @@ import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { useAppSelector } from '../../hooks';
+import { CourseState, getCourse } from '../../hooks/slices/course.slice';
 import { selectUser, UserState } from '../../hooks/slices/user.slice';
 import useCalcGrades from '../../hooks/useCalcGrades';
 import { Course } from '../../models/Course';
 import { CourseGrade } from '../../models/CourseGrade';
 import { User } from '../../models/User';
 import { getStudentCourses } from '../../remote/rev_learn_backend_api/RevLearnCoursesAPI';
-import { getActivityByID, getStudentSubmissions } from '../../remote/rev_learn_backend_api/RevLearnUsersAPI';
+import { getActivityByID, getCourseByID, getStudentSubmissions } from '../../remote/rev_learn_backend_api/RevLearnUsersAPI';
 import ActivitiesMap from './ActivitiesMap';
 
 type Props = {
 }
 
 const CourseGradesStudentView: React.FC<Props> = (props) => {
-  const [coursesList, setCourses] = useState<Course[]>();
   const [selected, setSelected] = useState<CourseGrade>();
   const nav = useNavigation();
   const user = useAppSelector<UserState>(selectUser);
+  const course = useAppSelector<CourseState>(getCourse);
   useEffect(() => {
     if(!user) {
       nav.navigate('Root');
-    } else {
-      console.log('useEffect hook');
-      (async () => {
-        const result = await getStudentCourses(user.id);
-        console.log('retrived course: ');
-        setCourses(result);
-      })();
     }
   }, []);
 
   return (
     <View>
       <Text>Grades for {}:</Text>
-      {user && coursesList
-      && coursesList.map((course, index) => (
-        <View key={index}>
+      {(user && course) && (
+        <View>
           <Text>Course: {course.courseTitle}:</Text>
           <Text>Assignments / Quizzes</Text>
           <ActivitiesMap Activities={course.activities}/>
         </View>
-      ))
+      )
       }
     </View>
   );
