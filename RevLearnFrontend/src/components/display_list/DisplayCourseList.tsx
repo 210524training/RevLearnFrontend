e52f6c25a4091fc1fc2cb18c450c7298714e9028
kirk simmonds <aldrich19@gmail.com>
2021-07-18 15:17:51 -0400

    add course request

--- RevLearnFrontend/src/components/display_list/DisplayCourseList.tsx
+++ RevLearnFrontend/src/components/display_list/DisplayCourseList.tsx
@@ -1,29 +1,43 @@
 import React, { Dispatch, SetStateAction, useState } from 'react';
 import {
+  Button,
   Image, ImageSourcePropType, StyleSheet, Text, View,
 } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { useAppSelector } from '../../hooks';
+import { selectUser, UserState } from '../../hooks/slices/user.slice';
 import { Course } from '../../models/Course';
+import { updateCourse } from '../../remote/rev_learn_backend_api/RevLearnCoursesAPI';
 
 type Props = {
   courses: Course[];
   setSelected: React.Dispatch<React.SetStateAction<Course[]>>;
 }
 
-const DisplayCourseList: React.FC<Props> = ({ courses, setSelected }) => (
-  <>
-    {courses.map((element, index) => (
-      <ListItem key={index}>
-        <Text>Course ID:  {`${element.id}`}</Text>
-        <Text>Course Title: {`${element.courseTitle}`}</Text>
-        <Text>Start Date: {`${element.startDate}`}</Text>
-        <Text>End Date: {`${element.endDate}`}</Text>
-        <Text>Teacher: {`${element.teacherID}`}</Text>
-        <Text>Passing Grade: {`${element.passingGrade}`}</Text>
-        <Text>Category: {`${element.category}`}</Text>
-      </ListItem>
-    ))}
-  </>
-);
+const DisplayCourseList: React.FC<Props> = ({ courses, setSelected }) => {
+  const user = useAppSelector<UserState>(selectUser);
+  const handleButton = (course: Course) => {
+    if(user) {
+      course.admissionRequests.push(user.id);
+      updateCourse(course);
+    }
+  };
+  return (
+    <>
+      {courses.map((element, index) => (
+        <ListItem key={index} >
+          <Text>Course ID: {`${element.id}`}</Text>
+          <Text>Course Title: {`${element.courseTitle}`}</Text>
+          <Text>Start Date: {`${element.startDate}`}</Text>
+          <Text>End Date: {`${element.endDate}`}</Text>
+          <Text>Teacher: {`${element.teacherID}`}</Text>
+          <Text>Passing Grade: {`${element.passingGrade}`}</Text>
+          <Text>Category: {`${element.category}`}</Text>
+          <Button onPress={() => handleButton(element)} title='Add Course'/>
+        </ListItem>
+      ))}
+    </>
+  );
+};
 
 export default DisplayCourseList;
