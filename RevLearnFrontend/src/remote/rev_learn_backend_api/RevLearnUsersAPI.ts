19b6a6dec9f0e97de4a439071be5c3ae1a81fefe
Sam Henley <samueljhenley@gmail.com>
2021-07-20 18:30:30 -0500

    clean up users api

--- RevLearnFrontend/src/remote/rev_learn_backend_api/RevLearnUsersAPI.ts
+++ RevLearnFrontend/src/remote/rev_learn_backend_api/RevLearnUsersAPI.ts
@@ -20,81 +20,6 @@ export const newStudent: User = {
   id: '151515',
 };
 
-const newStudent2: User = {
-  username: 'jon',
-  password: '123',
-  role: 'Student',
-  id: '123',
-};
-
-const assignmentSubmission: AssignmentSubmission = {
-  grade: 'A',
-  attachment: ['1', '1'],
-  submissionID: '2222',
-  studentID: '123',
-  submissionDate: new Date(Date.now()),
-};
-const assignmentSubmission1: AssignmentSubmission = {
-  grade: 'A',
-  attachment: ['1', '1'],
-  submissionID: '22',
-  studentID: '123',
-  submissionDate: new Date(Date.now()),
-};
-const assignment: Assignment = {
-  type: 'Homework',
-  title: 'my assignment',
-  submissions: [assignmentSubmission, assignmentSubmission1],
-  passingGrade: 'A',
-  ID: '112123',
-  startDate: new Date(Date.now()),
-  dueDate: new Date(Date.now()),
-  description: 'discription',
-};
-const assignment1: Assignment = {
-  type: 'Homework',
-  title: 'my assignment 1',
-  submissions: [assignmentSubmission, assignmentSubmission1],
-  passingGrade: 'A',
-  ID: '112123',
-  startDate: new Date(Date.now()),
-  dueDate: new Date(Date.now()),
-  description: 'discription',
-};
-const newvar2: Course = {
-  id: '1234',
-  courseTitle: 'Computer Science',
-  startDate: new Date(Date.now()).toDateString(),
-  endDate: new Date(Date.now()).toDateString(),
-  teacherID: 'ProfessorA',
-  passingGrade: 'C',
-  category: 'Software Engineering',
-  students: [],
-  activities: [
-    assignment,
-    assignment1,
-  ],
-  admissionRequests: [],
-};
-
-const newvar: Course = {
-  id: '3412',
-  courseTitle: 'Calculus',
-  startDate: new Date(Date.now()).toDateString(),
-  endDate: new Date(Date.now()).toDateString(),
-  teacherID: 'ProfessorC',
-  passingGrade: 'C',
-  category: 'Mathematic',
-  students: [],
-  activities: [
-    assignment,
-    assignment1,
-  ],
-  admissionRequests: [],
-};
-
-export const courses: Array<Course> = [newvar, newvar2];
-
 /**
  *
  * @param username
@@ -113,6 +38,7 @@ export const sendLogin = async (username: string, password: string): Promise<Use
   console.log(newStudent);
   return newStudent;
 };
+
 /**
  *
  * @callAStudent to use as a placeholder in replace of the original grubdashClient method in RegisterPage.
@@ -121,14 +47,32 @@ export function registerStudent() {
   return true;
 }
 
+// update password on settings page
+export function updatePassword(password: string, userID: string) {
+  console.log(password, userID);
+}
+
+export const addUser = async (username: string, password: string, role: string, id: string) => {
+  console.log(username, password, role, id);
+  await BackendClient.post('/user', {
+    username,
+    password,
+    role,
+    id,
+  }).then(() => console.log('Successfully Added User'))
+    .catch((err) => window.alert(err));
+};
+
 export function getAllUsers() {
   return BackendClient.get<User[]>('/user')
     .then((res) => { console.log('Successfully Found Users'); return res.data as User[]; })
     .catch((err) => { window.alert(err); return []; });
 }
 
-export function getByUserName() {
-  return newStudent;
+export function getAllTeachers() {
+  return BackendClient.get<User[]>('/user/teacher')
+    .then((res) => { console.log('Successfully Found Teachers'); return res.data as User[]; })
+    .catch((err) => { window.alert(err); return []; });
 }
 
 export async function getUserByID(id: string) {
@@ -143,57 +87,6 @@ export function deleteUser(id: string) {
     .catch((err) => window.alert(err));
 }
 
-/**
- * Use for updating a quiz with a new submission after the quiz is taken
- * @param quiz
- */
-export function updateQuiz(quiz: Quiz) {
-  //
-}
-export function getAllStudentsForCourse() {
-  const studentArray: User[] = [newStudent, newStudent2];
-  return studentArray;
-}
-
-export function getActivities(course: Course) {
-  console.log(course);
-
-  const submission: QuizSubmission = {
-    submissionID: '1',
-    studentID: '1',
-    submissionDate: new Date(),
-    grade: 80,
-  };
-
-  const quiz: Quiz = {
-    ID: '1',
-    startDate: new Date(),
-    dueDate: new Date(),
-    passingGrade: 75,
-    questions: [],
-    title: 'Quiz 1',
-    description: 'Description of quiz',
-    submissions: [submission],
-  };
-
-  return [quiz];
-}
-
-export function getActivityByID(id: string): Quiz | Assignment {
-  console.log(id);
-  const quiz: Quiz = {
-    ID: '1',
-    startDate: new Date(),
-    dueDate: new Date(),
-    passingGrade: 75,
-    questions: [],
-    title: 'Quiz 1',
-    description: 'Description of quiz',
-    submissions: [],
-  };
-
-  return quiz;
-}
 /**
  * Returns all of a student's submissions for a given course
  * @param course
@@ -211,53 +104,6 @@ export function getStudentSubmissions(course: Course, user: User): (QuizSubmissi
   };
 
   return [submission];
-}
-
-export function getCourseByID(id: string): Course {
-  const student: User = {
-    username: 'michael',
-    password: '123',
-    role: 'Student',
-    id: '456',
-  };
-
-  return {
-    id: '1234',
-    courseTitle: 'Calculus',
-    startDate: new Date(Date.now()).toDateString(),
-    endDate: new Date(Date.now()).toDateString(),
-    teacherID: 'ProfessorC',
-    passingGrade: 'C',
-    category: 'Mathematic',
-    students: [],
-    activities: [
-      assignment,
-      assignment1,
-    ],
-    admissionRequests: [student.id],
-  };
-}
-
-export function getAllTeachers() {
-  return BackendClient.get<User[]>('/user/teacher')
-    .then((res) => { console.log('Successfully Found Teachers'); return res.data as User[]; })
-    .catch((err) => { window.alert(err); return []; });
-}
-
-export const addUser = async (username: string, password: string, role: string, id: string) => {
-  console.log(username, password, role, id);
-  await BackendClient.post('/user', {
-    username,
-    password,
-    role,
-    id,
-  }).then(() => console.log('Successfully Added User'))
-    .catch((err) => window.alert(err));
-};
-
-// update password on settings page
-export function updatePassword(password: string, userID: string) {
-  console.log(password, userID);
 }
 
 export async function uploadFile(objectName: any, objectData: any) {
