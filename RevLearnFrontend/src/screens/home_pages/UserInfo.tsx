29140a3bc27ff17b9f84330b9796275b09dfe8a2
Sam Henley <samueljhenley@gmail.com>
2021-07-21 11:48:37 -0500

    hook up UserInfo page to redux

--- RevLearnFrontend/src/screens/home_pages/UserInfo.tsx
+++ RevLearnFrontend/src/screens/home_pages/UserInfo.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import WithHomeNavbar from '../../components/higher_order_components/Navbars/WithHomeNavbar';
+import { useAppSelector } from '../../hooks';
+import { UserState, selectUser } from '../../hooks/slices/user.slice';
 import { User } from '../../models/User';
 
 type Props = {
 
 }
-const UserInfoPage: React.FC<Props> = (props) => {
-  const user: User = {
-    username: 'DallinLemon',
-    password: 'Pass',
-    courses: [],
-    role: 'Student',
-    id: '1235',
-  };
+const UserInfoPage: React.FC<Props> = () => {
+  const user: User | null = useAppSelector<UserState>(selectUser);
   return (
     <>
       <Text>User Info Page</Text>
