7379696140aadff89168ec6c740f587a2f42714f
dallinlemon <dallinlemon@gmail.com>
2021-07-21 18:42:21 -0600

    fixed linting errors

--- RevLearnFrontend/src/components/web_navbar/DrawerIcon.tsx
+++ RevLearnFrontend/src/components/web_navbar/DrawerIcon.tsx
@@ -1,22 +1,12 @@
-/* eslint-disable arrow-body-style */
 import { Ionicons } from '@expo/vector-icons';
-import { DrawerNavigationProp } from '@react-navigation/drawer';
-import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Button } from 'react-native';
 
 const name: React.ComponentProps<typeof Ionicons>['name'] = 'menu';
-const color: string = 'black'
+const color: string = 'black';
 
 type Props = {
   navigation: any,
 }
-const DrawerIcon: React.FC<Props> = ({ navigation }) => {
-  // const navigation = useNavigation();
-  /* const openDrawer = () => {
-    navigation.
-  } */
-  return <Ionicons size={30} style={{ marginBottom: -3 }} name={name} color={color} onPress={() => navigation.toggleDrawer() }/>;
-};
+const DrawerIcon: React.FC<Props> = ({ navigation }) => <Ionicons size={30} style={{ marginBottom: -3 }} name={name} color={color} onPress={() => navigation.toggleDrawer() }/>;
 
 export default DrawerIcon;
